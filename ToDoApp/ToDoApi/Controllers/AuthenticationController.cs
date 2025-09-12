f97ba81638297d8fe9227e7614b4986fe57cb93c
Muhammed Shaheem <Shaheemofcl@gmail.com>
2025-09-12 18:05:48 +0530

    Added AUthentication and authorization to the api.

---
+++ ToDoApp/ToDoApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ToDoApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IConfiguration config;
+
+    public AuthenticationController(IConfiguration config)
+    {
+        this.config = config;
+    }
+
+    public record AuthenticationData(string? UserName, string? Password);
+    public record UserData(int Id, string FirstName, string LastName, string UserName);
+
+
+    [HttpPost("token")]
+    [AllowAnonymous]
+    public ActionResult<string> Authenticate([FromBody] AuthenticationData data)
+    {
+        UserData? user = ValidateCredentials(data);
+
+        if (user is null)
+        {
+            return Unauthorized();
+        }
+
+        string token = GenerateToken(user);
+        return Ok(token);
+
+
+    }
+
+    private string GenerateToken(UserData user)
+    {
+        var secretKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes((config.GetValue<string>("Authentication:SecretKey"))));
+        var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+        List<Claim> claims = new();
+        claims.Add(new(JwtRegisteredClaimNames.Sub, user.Id.ToString()));
+        claims.Add(new(JwtRegisteredClaimNames.UniqueName, user.UserName));
+        claims.Add(new(JwtRegisteredClaimNames.GivenName, user.FirstName));
+        claims.Add(new(JwtRegisteredClaimNames.FamilyName, user.LastName));
+
+        var token = new JwtSecurityToken(
+            config.GetValue<string>("Authentication:Issuer"),
+            config.GetValue<string>("Authentication:Audience"),
+            claims,
+            DateTime.UtcNow,
+            DateTime.UtcNow.AddMinutes(1),
+            signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+
+
+    }
+
+    private UserData? ValidateCredentials(AuthenticationData data)
+    {
+
+        if (CompareValues(data.UserName, "Shaheem") && CompareValues(data.Password, "Shaheem2003@"))
+        {
+            return new UserData(1, "Muhammed", "Shaheem", data.UserName!);
+        }
+        return null;
+    }
+
+    private bool CompareValues(string? actual, string expected)
+    {
+        if (actual is not null)
+        {
+            if (actual.Equals(expected))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
