737c997e903e1b164e32c2ac77c8f3b545186fb5
Muhammed Shaheem <Shaheemofcl@gmail.com>
2025-11-11 16:51:32 +0530

    Added Sql Data Access for the app

---
+++ ToDoApp/ToDoLibrary/DataAccess/ISqlDataAccess.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ToDoLibrary.DataAccess
+{
+    public interface ISqlDataAccess
+    {
+        Task<List<T>> LoadData<T, P>(string sql, P parameters, string connectionStringName);
+        Task SaveData<P>(string sql, P parameters, string connectionStringName);
+    }
+}
--- ToDoApp/ToDoLibrary/DataAccess/SqlDataAccess.cs
+++ ToDoApp/ToDoLibrary/DataAccess/SqlDataAccess.cs
@@ -1,6 +1,42 @@
-﻿namespace ToDoLibrary.DataAccess;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Threading.Tasks;
 
-public class SqlDataAccess
+namespace ToDoLibrary.DataAccess;
+
+public class SqlDataAccess : ISqlDataAccess
 {
+    private readonly IConfiguration configuration;
+
+    public SqlDataAccess(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public async Task<List<T>> LoadData<T, P>(string sql, P parameters, string connectionStringName)
+    {
+        string? connectionString = configuration.GetConnectionString(connectionStringName);
+        using IDbConnection connection = new SqlConnection(connectionString);
+
+        var rows = await connection.QueryAsync<T>(sql, parameters, commandType: CommandType.StoredProcedure);
+
+        return rows.ToList();
+
+
+    }
+
+    public Task SaveData<P>(string sql, P parameters, string connectionStringName)
+    {
+        string? connectionString = configuration.GetConnectionString(connectionStringName);
+        using IDbConnection connection = new SqlConnection(connectionString);
+
+        return connection.ExecuteAsync(sql, parameters, commandType: CommandType.StoredProcedure);
+
+
+
+
+    }
 
 }
